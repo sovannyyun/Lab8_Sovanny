119a8cf3021562d9231b4037bd99f2c9d0249d47
sovannyyun <sovanny.yun.cis@gmail.com>
2018-03-17 16:33:40 -0700

    Initial Commit

---
+++ Sovanny_Yun_CST356_Lab8/MyWebApi/Controllers/MajorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sovanny_Yun_CST356_Lab3.Data;
+using Sovanny_Yun_CST356_Lab3.Data.Entities;
+
+namespace MyWebApi.Controllers
+{
+    [RoutePrefix("api/Majors")]
+    public class MajorController : ApiController
+    {
+
+        private AppDbContext _dbContext;
+
+        public MajorController()
+        {
+            _dbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Major> GetAllMajors()
+        {
+            return _dbContext.Majors.ToList();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IHttpActionResult GetMajors(int id)
+        {
+            var major = _dbContext.Majors.FirstOrDefault((p) => p.Id == id);
+            if (major == null)
+            {
+                return NotFound();
+            }
+            return Ok(major);
+        }
+    }
+}
---
+++ Sovanny_Yun_CST356_Lab8/MyWebApi/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace MyWebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AppDomain.CurrentDomain.SetData("DataDirectory", @"C:\Users\SovannyTfk\Documents\Academic\CST356\Labs\Sovanny_Yun_CST356_Lab8\Sovanny_Yun_CST356_Lab3\App_Data");
+        }
+    }
+}
